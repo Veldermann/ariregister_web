b32e1a2c39014a6b06a63cdfe4417196de5df0a2
Kerth Veldermann <kerth.veldermann@taltech.ee>
2023-05-01 20:18:33 +0300

    1 launch file to create tables and fill person table

--- scripts/generate_companies.py
+++
@@ -1,7 +0,0 @@
-from database_connection import getDatabaseConnection
-
-def main():
-    return
-
-if __name__ == "__main__":
-    main()
---
+++ scripts/launchMe.py
@@ -0,0 +1,10 @@
+from person_generator import generatePersonData
+from generate_tables import createTables
+
+def main():
+    createTables()
+    generatePersonData()
+    return
+
+if __name__ == '__main__':
+    main()
